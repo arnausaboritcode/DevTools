522f8354e26ce09ac5e3c1ce2c089485510c0f1b
Arnau Saborit <arnausv@uoc.edu>
2024-09-24 12:20:12 +0200

    link and port changed

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  BASE_API_URL_BACKEND: 'http://localhost:3000',
-  GEMINI_API_KEY: 'AIzaSyCURwg7N8fVdLs9nxrd4rBD9Z-iEy2my3c',
+  BASE_API_URL_BACKEND: 'https://devtools-w1w5.onrender.com',
 };
