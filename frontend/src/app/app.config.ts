aa448ebc10d6557fc4b3964a84cd5bdf45b4f012
Arnau Saborit <arnausv@uoc.edu>
2024-09-25 17:12:46 +0200

    Vercel analytics added

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { provideHttpClient } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { inject } from '@vercel/analytics';
 import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
 
@@ -14,5 +15,14 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideAnimations(),
     provideToastr(),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => {
+        inject({
+          mode: isDevMode() ? 'development' : 'production',
+          debug: false,
+        });
+      },
+    },
   ],
 };
